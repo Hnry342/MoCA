62651565ff7d5ba58c1cd9807bceff6555b99308
Hnry342 <hnry342@gmail.com>
2020-11-01 19:10:21 +0000

    Question 1
    Question 1 pass if correct sequence is followed

---
+++ MoCA/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+   // public static CanvasManager instance;
+
+    public GameObject CanvasQ1;
+    public GameObject CanvasQ2;
+    public GameObject CanvasQ3;
+    // Start is called before the first frame update
+ /*    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    } */
+
+    void Start()
+    {
+        PlayerPrefs.SetInt("currentQuestion", 1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if ( PlayerPrefs.GetInt("currentQuestion") == 1)
+        {
+            Question1();
+        }
+        if ( PlayerPrefs.GetInt("currentQuestion") == 2)
+        {
+            Debug.Log("QUESTION 2");
+            Question2();
+        }
+
+    }
+
+    public void Question1()
+        {
+            
+            CanvasQ1.SetActive(true);
+            CanvasQ2.SetActive(false);
+            CanvasQ3.SetActive(false);
+
+
+        }
+    public void Question2()
+        {
+            CanvasQ1.SetActive(false);
+            CanvasQ2.SetActive(true);
+            CanvasQ3.SetActive(false);
+        }
+    public void Question3()
+        {
+            CanvasQ1.SetActive(false);
+            CanvasQ2.SetActive(false);
+            CanvasQ3.SetActive(true);
+        }
+}
---
+++ MoCA/Assets/Scripts/TouchObject.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchObject : MonoBehaviour
+{
+    bool mouseHeld = false;
+    string[ ] correctSequence = new string[] {"1", "A", "2", "B", "3", "C", "4","D","5","E"} ;
+    public int correct = 0; 
+    public int Qcount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check for mouse up and down
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseHeld = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            mouseHeld = false;
+        }
+
+        //Collision
+          //Declare collision raycast
+        Vector2 circleRay = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D circleHit = Physics2D.Raycast(circleRay, Vector2.zero);
+        //if conditions for collisions
+        if (circleHit && circleHit.collider.enabled && mouseHeld)
+        {
+            //If collider Name == Correct Sequence
+            if (circleHit.collider.name == correctSequence[Qcount])
+            {
+                Debug.Log("Correct! " + circleHit.collider.name + " correct answer is " + correctSequence[Qcount]);
+                //+1 on correct score
+                correct++;
+                //Disable collider after touch
+                circleHit.collider.enabled = !circleHit.collider.enabled;
+            }
+            else if (circleHit.collider.name != correctSequence[Qcount])
+            {
+                Debug.Log("Wrong! " + circleHit.collider.name + " correct answer is " + correctSequence[Qcount]);
+                //Disable collider after touch
+                circleHit.collider.enabled = !circleHit.collider.enabled;
+            }
+            //Increase Count
+            Qcount++;
+        }
+        //Check if Qcount = 10
+        if (Qcount >= 10)
+        {
+            //check if score is 10 (pass) or less (fail)
+           if (correct >= 10)
+           {
+            Debug.Log("Pass ");
+            PlayerPrefs.SetInt("Q1", 1);
+
+           }
+           else if (correct < 10)
+           {
+            Debug.Log("Fail ");
+            PlayerPrefs.SetInt("Q1", 0); 
+           }
+           //Reset count
+            //Qcount = 0;
+            //correct = 0; 
+            Debug.Log( PlayerPrefs.GetInt("Q1").ToString());
+            PlayerPrefs.SetInt("currentQuestion", 2);
+
+        }
+    }
+
+      void OnMouseOver()
+    {
+      
+    }
+}
