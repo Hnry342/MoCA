117475303e9b18b06928ab049477d05b189b1de3
Hnry342 <hnry342@gmail.com>
2020-11-15 13:41:39 +0000

    Question 5
    Removed white space from text stream, dsiabled sprite renderer instead of gameobject to move on to next animal

--- MoCA/Assets/Scripts/SpeechManager.cs
+++ MoCA/Assets/Scripts/SpeechManager.cs
@@ -11,35 +11,46 @@ public class SpeechManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (PlayerPrefs.GetInt("currentQuestion").ToString()== "4")
-        {
-            GameObject.Find("Rhino").SetActive(false);
-            GameObject.Find("Camel").SetActive(false);
-        }
+        //if (PlayerPrefs.GetInt("currentQuestion").ToString()== "4")
+        //{
+            GameObject.Find("Rhino").GetComponent<SpriteRenderer>().enabled = false;
+            GameObject.Find("Camel").GetComponent<SpriteRenderer>().enabled = false;
+       // }
     }
 
     // Update is called once per frame
     void Update()
     {
-        var sr = File.CreateText(fileName);
-        sr.WriteLine (GameObject.Find("TextStreaming").GetComponent<Text>().text);
-        sr.Close();
-
-       // if (GameObject.Find("Lion") == true)
-       // {
-            string result = GameObject.Find("TextStreaming").GetComponent<Text>().text;
-            if (result == "lion")
-            {
-                Debug.Log("Success");
-                GameObject.Find("Lion").SetActive(false);
-                GameObject.Find("Rhino").SetActive(true);
-            }
-        //}
-        else if (GameObject.Find("Rhino") == false)
+///////////Question 4/////////////////////
+        if (PlayerPrefs.GetInt("currentQuestion").ToString()== "4")
         {
+            if (GameObject.Find("Lion").GetComponent<SpriteRenderer>().enabled == true)
+            {
+                if (GameObject.Find("TextStreaming").GetComponent<Text>().text.Trim() == "lion")
+                {
+                    GameObject.Find("Lion").GetComponent<SpriteRenderer>().enabled = false;
+                    GameObject.FindWithTag("Rhino").GetComponent<SpriteRenderer>().enabled = true;
+                }
+            }
+            else if (GameObject.Find("Rhino").GetComponent<SpriteRenderer>().enabled == true)
+            {
+                if (GameObject.Find("TextStreaming").GetComponent<Text>().text.Trim() == "Rhino")
+                {
+                    GameObject.Find("Rhino").GetComponent<SpriteRenderer>().enabled = false;
+                    GameObject.FindWithTag("Camel").GetComponent<SpriteRenderer>().enabled = true;
+                }
 
+            }
+            else if (GameObject.Find("Camel").GetComponent<SpriteRenderer>().enabled == true)
+            {
+                if (GameObject.Find("TextStreaming").GetComponent<Text>().text.Trim() == "Camel")
+                {
+                    GameObject.Find("Camel").GetComponent<SpriteRenderer>().enabled = false;
+                    PlayerPrefs.SetInt("currentQuestion", 5);
+                }
+
+            }
         }
-
-
+////////////////////////////////////////Question 4 end//////////////////
     }
 }
